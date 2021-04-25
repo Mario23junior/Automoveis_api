17680bf42263b7d3c451dd7546c66af97dedcbe6
Mario23junior <mariojunior3251@gmail.com>
2021-04-25 02:47:07 -0300

    Salvando automoveis no banco de dados

--- src/main/java/com/project/carros/Controllers/CarrosController.java
+++ src/main/java/com/project/carros/Controllers/CarrosController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +38,9 @@ public class CarrosController {
 		return service.listByType(tipo);
 	}
 	
+	@PostMapping
+	public void save(@RequestBody Carro carro) {
+		 service.save(carro);
 	}
+
+}
