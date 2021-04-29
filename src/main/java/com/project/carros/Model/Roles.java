3ec52b59f895d01060d048b3692e315dd598d094
Mario23junior <mariojunior3251@gmail.com>
2021-04-29 05:59:54 -0300

    Corrigindo bug das breach #34 #35

--- src/main/java/com/project/carros/Model/Roles.java
+++ src/main/java/com/project/carros/Model/Roles.java
@@ -5,19 +5,14 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
-public class User {
+public class Roles {
     
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
-	private String login;
-	private String senha;
-	private String email;
 
-	
 	public String getNome() {
 		return nome;
 	}
@@ -30,22 +25,5 @@ public class User {
 	public void setId(Long id) {
 		this.id = id;
 	}
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getSenha() {
-		return senha;
-	}
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 }
